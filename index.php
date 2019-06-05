ee239ae594b3183dbfebe08b3609047787bc6346
Ian Hulston <hulstonian@gmail.com>
2019-06-05 21:57:33 +0100

    First initial commit of index header/navbar.

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Breakdown Recovery Home</title>
+	<link rel="stylesheet" type="text/css" href="styles/main-style.css">
+</head>
+<body>
+	<div class="header">
+	<span class="navtitle">Breakdown Recovery Service</span>
+		<div class="header-right">
+			<a class="active" href="#home">Home</a>
+			<a href="#services">Services</a>
+			<a href="#contact">Contact</a>
+			<a href="sign-up.html">Register</a>
+			<div class="login-container">
+				<form action="login.php">
+					<input type="email" placeholder="Email" name="email">
+					<input type="password" placeholder="Password" name="psw">
+					<button type="submit">Login</button>
+				</form>
+		</div>
+	</div>
+
+	<img src="images/thumbs-up-recovery.png" alt="Main Image." class="mainImage">
+
+	<!-- <div id="mainContent">
+		<div class="breakdownNews">
+			<h4>Breakdown News</h4>
+			<p>All the latest news from breakdown services, etc..</p>
+		</div>
+		<div class="breakdownCover">
+			<h4>Breakdown Covers</h4>
+			<p>All the breakdown covers we provide to our customers, etc..</p>
+		</div>
+	</div>
+
+	<div class="registerReminderSection">
+		<p>Register now</p>
+	</div> -->
+
+	<footer>
+		<div class="footerLinks">
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+		</div>
+	</footer>
+</body>
+</html>
